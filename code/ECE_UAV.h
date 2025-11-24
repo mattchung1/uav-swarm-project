226eb74b341ddf63f876100c57787e2ee52d4c6e
Matt Chung <mhc88168@gmail.com>
2025-11-23 20:27:51 -0500

    Adding basic structure for header files

---
+++ code/ECE_UAV.h
@@ -0,0 +1,28 @@
+/*
+Header file for ECE_UAV class
+
+*/
+
+
+#pragma once
+
+class ECE_UAV {
+    private:
+        // variables used in the class
+        
+
+    public:
+        //Declare member functions
+        ECE_UAV(float startX, float startY);
+        ~ECE_UAV();
+
+        // Get position of UAV
+        // Get velocity of UAV
+        // Get acceleration of UAV
+
+        // Update kinematics
+        // Check collision with other UAVs
+
+
+
+};
